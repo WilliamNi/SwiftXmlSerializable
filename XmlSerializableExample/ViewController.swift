e3e7acd3b347bc89894d10e6af1129fb64e15f2e
wni3 <weibo.ni@intel.com>
2015-09-16 15:47:49 -0700

    [William Ni]: Add init folder structure.

---
+++ XmlSerializableExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XmlSerializable
+//
+//  Created by ixprt13 on 9/16/15.
+//  Copyright © 2015 williamni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
